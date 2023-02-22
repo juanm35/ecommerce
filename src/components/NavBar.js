d66c798aa5927325162d28440f4a7cb00e98c1aa
juanm35 <jjmigone@gmail.com>
2023-02-22 11:48:09 -0300

    fixing styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,14 +5,14 @@ import CartWidget from './Cart/CartWidget.js'
 export default function NavBar() {
 
     return(
-        <nav className="flex text-3xl justify-between px-16 py-2 text-white bg-jungle-grey items-center">
-            <img src={logo} className="w-1/6" alt="log jungle" />
-            <div className='flex gap-6 w-2/3 justify-center h-full'>
-                <div className='hover:text-jungle-green'>Art</div>
-                <div className='hover:text-jungle-green'>Gaming</div>
-                <div className='hover:text-jungle-green'>Photography</div>
+        <nav className="flex text-3xl justify-between px-16 py-2 text-white bg-jungle-grey h-20 items-center">
+            <img src={logo} className="w-1/6 max-h-full cursor-pointer" alt="log jungle" />
+            <div className='flex gap-x-6 w-2/3 h-full cursor-pointer'>
+                <div className='hover:text-jungle-green h-full p-2 flex items-center'>Art</div>
+                <div className='hover:text-jungle-green h-full p-2 flex items-center'>Gaming</div>
+                <div className='hover:text-jungle-green h-full p-2 flex items-center'>Photography</div>
             </div>
-            <CartWidget className="w-1/6" />
+            <CartWidget className="w-1/6 cursor-pointer " />
             
         </nav>
     )
